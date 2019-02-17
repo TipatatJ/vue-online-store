220de38ae1649ab6f9d3b0bfe9bc4964e353b02c
Pearl Zeng <zhuzeng@umich.edu>
2019-02-16 21:12:47 -0500

    Display products data on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
+  <div>
+    <section class="wrapper">
+      <h2>Featured Items</h2>
+      <ul class="featured-items">
+        <li
+          v-for="product in products"
+          :key="product.id"
+          class="featured-items__item"
+        >
+          <img class="product-image" :src="imagePath(product)" alt="" />
+          <p class="product-title">{{ product.name }}</p>
+          <p>
+            <em>${{ product.price }}</em>
+          </p>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: "home",
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  methods: {
+    imagePath(product) {
+      return require(`../assets/img/products/${product.images[0]}`);
+    }
   }
 };
 </script>
