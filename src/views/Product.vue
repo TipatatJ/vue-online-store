1651eea2e56d5fbbcce8c49466990b0f2a6cfe6c
Pearl Zeng <zhuzeng@umich.edu>
2019-02-17 10:08:12 -0500

    Flesh out single product page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
     <section class="wrapper">
-      <h2>{{ product.name }}</h2>
+      <div class="flex-col">
+        <img
+          class="flex-col--2"
+          :src="imagePath(product)"
+          alt="product image"
+        />
+        <div class="flex-col--2">
+          <h2>{{ product.name }}</h2>
+          <p>Prize: ${{ product.price }}</p>
+          <p>Size: {{ product.size }}</p>
+          <p>Color: {{ product.color }}</p>
+          <p>
+            <em>{{ product.quantity }} left in stock</em>
+          </p>
+          <h3>Details</h3>
+          <ul>
+            <li>Material: {{ product.details.material }}</li>
+            <li>Fit: {{ product.details.fit }}</li>
+            <li>Maintenance: {{ product.details.maintenance }}</li>
+            <li v-if="product.details.additional">
+              Additional information: {{ product.details.additional }}
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -13,8 +37,22 @@ export default {
     return {
       product: this.$store.getters.product(this.$route.params.id)
     };
+  },
+  methods: {
+    imagePath(product) {
+      return require(`../assets/img/products/${product.images[0]}`);
+    }
   }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.flex-col {
+  display: flex;
+  align-items: flex-start;
+}
+
+.flex-col--2 {
+  width: 50%;
+}
+</style>
