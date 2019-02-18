e982e65430801ab763e11fc3083849a8c3c0e9c6
Pearl Zeng <zhuzeng@umich.edu>
2019-02-18 10:21:12 -0500

    Flesh out item display on cart page

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -2,16 +2,27 @@
   <div class="wrapper">
     <h1>Cart</h1>
     <ul>
-      <li v-for="item in cartItems">
-        {{ item.name }}
+      <li v-for="item in cartItems" class="flex-col cart-list__item">
+        <img :src="imagePath(item)" class="thumbnail" alt="product thumnail" />
+        <div class="flex-col cart-list__item__details">
+          <div>
+            <p>{{ item.name }}</p>
+            <p>Size: {{ item.size }}</p>
+            <p>Color: {{ item.color }}</p>
+          </div>
+          <p>${{ item.price }}</p>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { imagePath } from "@/mixins/imagePath.js";
+
 export default {
   name: "cart",
+  mixins: [imagePath],
   computed: {
     cartItems() {
       return this.$store.getters.cartItems;
@@ -19,3 +30,29 @@ export default {
   }
 };
 </script>
+
+<style>
+.cart-list {
+  width: 70%;
+  margin-right: 1rem;
+  @media only screen and (max-width: 832px) {
+    width: 100%;
+  }
+}
+
+.cart-list__item {
+  width: 100%;
+  border-bottom: 1px solid #2c3e50;
+}
+
+.cart-list__item__details {
+  flex: 2;
+  justify-content: space-between;
+  margin-left: 2rem;
+}
+
+.thumbnail {
+  max-width: 50px;
+  margin-top: 0.5rem;
+}
+</style>
