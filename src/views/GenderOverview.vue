502f382e773d60a980941e7e1e4c953a75d62e42
Pearl Zeng <zhuzeng@umich.edu>
2019-02-18 13:39:09 -0500

    Flesh out products by gender page

--- src/views/GenderOverview.vue
+++ src/views/GenderOverview.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
     <h1 class="wrapper">{{ pageTitle }}</h1>
-    <ul>
-      <li v-for="product in productsByGender">{{ product.name }}</li>
+    <ul class="wrapper item-grid">
+      <li
+        v-for="product in productsByGender"
+        :key="product.id"
+        class="item-grid__item"
+      >
+        <router-link :to="{ name: 'product', params: { id: product.id } }">
+          <img
+            class="product-image"
+            :src="imagePath(product)"
+            alt="product image"
+          />
+          <p class="product-title">{{ product.name }}</p>
+          <p>
+            <em>${{ product.price }}</em>
+          </p> </router-link
+        >}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { imagePath } from "@/mixins/imagePath.js";
+
 export default {
   name: "GenderOverview",
+  mixins: [imagePath],
   computed: {
     gender() {
       return this.$route.params.gender;
